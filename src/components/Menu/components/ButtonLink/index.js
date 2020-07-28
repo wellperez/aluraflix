77d87dbe5428497603ec06685e79f96f2e43c211
Wellington Perez <wellingtonx3@hotmail.com>
2020-07-28 15:15:58 -0300

    Finished first lesson

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+
+// // import { Container } from './styles';
+
+// function ButtonLink(props) {
+//   // props => {}
+//   console.log(props);
+//   return (
+//     <a href={props.href} className={props.className} >{props.children}</a>
+//     );
+// }
+
+// export default ButtonLink;
