b5369ce949d71bf7242f7cc9286d6c9551503ca5
Wellington Perez <wellingtonx3@hotmail.com>
2020-07-29 14:34:41 -0300

    Change main app and added category and video pages

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
 
+import {Link} from 'react-router-dom'
+
 
 // import { Container } from './styles';
 //import ButtonLink from './components/ButtonLink/index';
@@ -10,11 +12,11 @@ import Button from '../Button/';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="Fofoflix Logo"/>
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </nav>
