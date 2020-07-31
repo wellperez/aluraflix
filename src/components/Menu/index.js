758fce26aaddc04717e484ee22ae98dab76bb43e
Wellington Perez <wellingtonx3@hotmail.com>
2020-07-31 10:56:07 -0300

    Chaging form field style

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
 
-import {Link} from 'react-router-dom'
-
-
-// import { Container } from './styles';
-//import ButtonLink from './components/ButtonLink/index';
-import Button from '../Button/';
+import Button from '../Button';
 
 function Menu() {
   return (
     <nav className="Menu">
       <Link to="/">
-        <img className="Logo" src={Logo} alt="Fofoflix Logo"/>
+        <img className="Logo" src={Logo} alt="Fofoflix Logo" />
       </Link>
 
       <Button as={Link} className="ButtonLink" to="/cadastro/video">
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -93,7 +93,7 @@ function CadastroCategoria() {
           </label>
         </div> */}
 
-        <Button>
+        <Button as="a" className="ButtonLink">
           Cadastrar
         </Button>
       </form>
